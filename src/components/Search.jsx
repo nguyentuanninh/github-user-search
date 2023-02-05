a0f0a1201fa7f5f8dfe2d7f266af44579bbd9203
ninh <nguyentuanninh02@gmail.com>
2023-02-06 01:02:13 +0700

    2_6_2023

---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+Reflect;
+import React from "react";
+
+const Search = () => {
+    return <div>Search</div>;
+};
+
+export default Search;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,23 @@ module.exports = {
     darkMode: "class",
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            color: {
+                primary: "#B9F3FC",
+                secondary: "#AEE2FF",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            screens: {
+                xs: "480px",
+                ss: "620px",
+                sm: "768px",
+                md: "1060px",
+                lg: "1200px",
+                xl: "1700px",
+            },
+        },
     },
     plugins: [],
 };
